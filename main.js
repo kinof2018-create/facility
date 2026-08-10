42008306be216e1342d09deed8b96195a58eba8b
kinof2018-create <kinof2018@gmail.com>
2026-08-10 11:19:34 +0900

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-const menuToggle = document.getElementById("menuToggle");
-const mainNav = document.getElementById("mainNav");
-
-if (menuToggle && mainNav) {
-  menuToggle.addEventListener("click", () => {
-    const isOpen = mainNav.classList.toggle("open");
-    menuToggle.setAttribute("aria-expanded", String(isOpen));
-  });
-}
